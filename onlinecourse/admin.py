2860adfd16f07bc6422965b2d4c9e32e02332f86
Zahra Ramezani <ramezani_zr@yahoo.com>
2022-11-17 17:18:20 -0500

    add classes to model

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
@@ -21,10 +21,25 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+ 
+class QuestionInline(admin.StackedInline):
+    model = Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('text', 'grade')
+    search_fields = ['text']
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('text','is_correct')
 
 # <HINT> Register Question and Choice models here
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
